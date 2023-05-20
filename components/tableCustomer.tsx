0a580e2aba9bead2e7b0782775b12a22119c1ea2
dsypasit <ong.pasit@gmail.com>
2023-05-20 10:42:22 +0700

    feat: picup customer by post code

--- components/tableCustomer.tsx
+++ components/tableCustomer.tsx
@@ -1,12 +1,20 @@
 import React from "react"
 import '../app/globals.css'
 
+enum Pickup{
+    upcountry = "upcountry",
+    makesend = "makesend",
+    vicinity = "vicinity"
+}
+
+
 interface Customer{
     id: number
     name: string
     weight: number
     post_code: string
     size: string
+    pickup?: Pickup
 }
 
 type TableProps = {
@@ -16,24 +24,25 @@ type TableProps = {
 
 export default function Table (props: TableProps){
     return (
-    <table className="text-left text-sm font-light">
+    <table className="text-left text-sm font-light min-w-fit">
         <thead>
             <tr className="border-b font-medium dark:border-neutral-500">
             {props.head.map((e, i) => (
                 <th className="px-6 py-4" key={i}>{e}</th>
             ))}
             </tr>
-        </thead>
-        <tbody>
-        {props.body.map((e, i) => ( <tr className="border-b dark:border-neutral-500" key={i}>
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">{e.id}</td>
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">{e.name}</td>
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">{e.weight}</td>
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">{e.post_code}</td>
-                    <td className="whitespace-nowrap px-6 py-4 font-medium">{e.size}</td>
-                    </tr>
-                    ))}
-        </tbody>
-    </table>
-    )
+            </thead>
+            <tbody>
+            {props.body.map((e, i) => (
+                        <tr className={`border-b dark:border-neutral-500 ${e.pickup === Pickup.upcountry ? 'bg-red-300' : (e.pickup === Pickup.makesend ? 'bg-sky-300' : (e.pickup === Pickup.vicinity ? 'bg-green-300' : ''))}` } key={i}>
+                        {Object.entries(e).map(([k, v]) => (
+                                    <td className="whitespace-nowrap px-6 py-4 font-medium" key={k}>
+                                    {v}
+                                    </td>
+                                    ))}
+                        </tr>
+                        ))}
+            </tbody>
+            </table>
+            )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,21 @@ import locationData from '../mock_data/post_code.json'
 import Table from '../components/tableCustomer'
 import '../app/globals.css'
 
+enum Car{
+    CurierBike = "Curier Bike",
+    sedan = "sedan",
+    hatchBack = "hatch back",
+    SUV = "SUV",
+    kraba = "กระบะ",
+    krabaTub = "กระบะทึบ"
+}
+
+enum Pickup{
+    upcountry = "upcountry",
+    makesend = "makesend",
+    vicinity = "vicinity"
+}
+
 interface Person {
     userName: string
 }
@@ -14,6 +29,8 @@ interface Customer{
     weight: number
     post_code: string
     size: string
+    location: string
+    pickup?: Pickup
 }
 
 interface Location{
@@ -24,6 +41,11 @@ interface Location{
     post_code: string
 }
 
+interface CustomerGroup{
+    post_code: string
+    customers: Customer[]
+}
+
 let customer_name_list: Array<Person> = JSON.parse(JSON.stringify(mockData));
 let location_list: Array<Location> = JSON.parse(JSON.stringify(locationData));
 
@@ -31,31 +53,90 @@ function randomRange(end:number): number{
     return Math.floor(Math.random()*end+1)
 }
 
-function mockCustomer(name_list: Array<Person>): Customer[]{
-    let customer_list: Customer[] = name_list.map((e, i) => ({
-            id : i,
-            name : e.userName,
-            weight : randomRange(1100),
-            size : randomRange(170).toString() +'x' + randomRange(210).toString()+'x' + randomRange(210).toString(),
-            post_code : location_list[randomRange(location_list.length-1)].post_code
-            }))
-    console.log(customer_list)
-    return customer_list
+function bangkokAndVicinityCode(location: Location[]) :string[]{
+    let bangkok_and_vicinity = ["กรุงเทพมหานคร", "นนทบุรี", "สมุทรปราการ", "ปทุมธานี"]
+    return location.filter(e => bangkok_and_vicinity.includes(e.city)).map(e => e.post_code)
 }
 
+function isBangkokAndVicinity(customer: Customer): boolean{
+    return bangkokAndVicinityCode(location_list).includes(customer.post_code)
+}
+
+function groupCustomerByPostCode(customers: Customer[]): CustomerGroup[]{
+    let customerPostCodes = customers.map(e => (e.post_code))
+    let uniqueCustomerPostCodes = new Set(customerPostCodes)
+
+    let customerGroups: CustomerGroup[] = Array.from(uniqueCustomerPostCodes).map(loc => ({
+        post_code: loc,
+        customers: customers.filter(e => e.post_code === loc)
+                }));
+    return customerGroups
+}
+
+function splitSize(size: string): number[]{
+    return size.split("x").map(e => parseInt(e))
+}
+
+function pickup(customer: Customer): Pickup{
+    if (!isBangkokAndVicinity(customer)){
+        return Pickup.upcountry
+    }else if(customer.weight<=25 && splitSize(customer.size).reduce((pre, cur) => pre+cur, 0) <= 200){
+        return Pickup.makesend
+    }
+    return Pickup.vicinity
+}
+
+function pickupCustomers(customers: Customer[]): Customer[]{
+    return customers.map(e => ({
+        ...e, pickup: pickup(e)
+                }))
+}
+
+// function pickupVicinity(customer: Customer): Cars{
+//
+// }
+
+function mockCustomer(name_list: Array<Person>): Customer[]{
+    let customer_list: Customer[] = name_list.map((e, i) => { 
+            const locIndex = randomRange(location_list.length-1);
+            return {
+                id : i,
+                name : e.userName,
+                weight : randomRange(100),
+                size : randomRange(170).toString() +'x' + randomRange(210).toString()+'x' + randomRange(210).toString(),
+                post_code : location_list[locIndex].post_code,
+                location: location_list[locIndex].city
+            } ;
+    });
+    return customer_list.slice(0, 10)
+    }
+
 export default function Index(){
-    let head: Array<string> = ["id", "name", "weight", "post code", "size"];
     let [customers, setCustomers] = useState<Array<Customer>>([]);
+    let [head, setHead] = useState<string[]>(["id", "name", "weight", "post code", "size", "location"]);
     useEffect((()=>{
                 setCustomers(mockCustomer(customer_name_list));
                 }), [])
 
     const Mock = () => {
-        setCustomers(mockCustomer(customer_name_list))
+        setCustomers(pickupCustomers(mockCustomer(customer_name_list)))
+        setHead(["id", "name", "weight", "post code", "size", "location", "group"])
+    }
+
+    const updateTable = () => {
+        setCustomers(pickupCustomers(customers))
+        setHead(["id", "name", "weight", "post code", "size", "location", "group"])
+            console.log(head)
+            console.log(customers)
     }
 
     return <>
-        <button onClick={Mock}>Mock user</button>
-        <Table head={head} body={customers} />
+        <div className="flex w-full justify-center mt-10">
+        <button className='bg-sky-500 mr-4 px-5 py-5 rounded-lg' onClick={Mock}>Mock user</button>
+        <button className='bg-sky-500 px-5 rounded-lg' onClick={updateTable}>Group</button>
+        </div>
+        <div className='flex justify-center mt-5'>
+            <Table head={head} body={customers} />
+        </div>
         </>
 }
