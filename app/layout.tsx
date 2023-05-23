da2dacbe6f0789a7c7960a29bcd44e9292afa764
Pasit Sri-intarasut <ong.pasit@gmail.com>
2023-05-23 09:56:15 +0700

    fix: gtm script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -17,29 +16,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script>
-          {`
-          <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PZD5TNF');</script>
-<!-- End Google Tag Manager -->
-          `}
-        </Script>
+})(window,document,'script','dataLayer','GTM-PZD5TNF');`}</script>
       </head>
       <body className={inter.className}>
-        <Script>
-        {`
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZD5TNF"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-        `}
-        </Script>   
-        {children}
-      </body>
+<noscript>{`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZD5TNF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}</noscript>
+        {children}</body>
     </html>
   )
 }
