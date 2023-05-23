9116294496e4241146a83edd61a20b0f84a6283a
Pasit Sri-intarasut <ong.pasit@gmail.com>
2023-05-23 14:12:12 +0700

    fix gtm 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,16 @@ import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }:AppProps) {
   return <>
-    <Head>
-       <Script id="gtm" strategy="afterInteractive">
-        {`
+       <Script id="gtm" strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-PZD5TNF');
-        `}
+        `
+       }}>
       </Script>
-    </Head>
     <body>
       <noscript>
         <iframe
@@ -27,7 +26,7 @@ export default function MyApp({ Component, pageProps }:AppProps) {
           }}
         ></iframe>
       </noscript>
-<Component {...pageProps} />
+  <Component {...pageProps} />
     </body>
   </>
 }
