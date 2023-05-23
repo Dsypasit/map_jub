9e43d42e6c9e4ec004fae7304c8d6f09c7936255
Pasit Sri-intarasut <ong.pasit@gmail.com>
2023-05-23 10:24:58 +0700

    fix gtm by move code from layout to _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import { AppProps } from "next/app";
+import Script from 'next/script';
+
+export default function MyApp({ Component, pageProps }:AppProps) {
+  return <>
+       <Script id="gtm" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PZD5TNF');
+        `}
+      </Script>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PZD5TNF"
+          height="0"
+          width="0"
+          style={{
+            display: 'none',
+            visibility: 'hidden',
+          }}
+        ></iframe>
+      </noscript>
+<Component {...pageProps} />
+  </>
+}
